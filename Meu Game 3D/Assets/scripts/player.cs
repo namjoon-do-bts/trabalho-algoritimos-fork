f3b0828b4f4ff62b79fb0b79cc1a33e7d7747f47
namjoon-do-bts <flor.l@escolar.ifrn.edu.br>
2024-08-27 14:23:04 -0300

    jogo pra consertar

--- Meu Game 3D/Assets/scripts/player.cs
+++ Meu Game 3D/Assets/scripts/player.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEditor.SearchService;
@@ -8,11 +9,18 @@ public class player : MonoBehaviour
 {
     public int velocidade = 10;
     Rigidbody rb;
+    public int forcePulo = 10;
     // Start is called before the first frame update
     void Start()
     {
       TryGetComponent(out rb);
     }
+
+    private void OnCollisionEnter(Collision other)
+    {
+        throw new NotImplementedException();
+    }
+
 
     // Update is called once per frame
     void Update()
@@ -21,6 +29,12 @@ float h = Input.GetAxis("Horizontal");
 float v = Input.GetAxis("Vertical");
 Vector3 direcao = new Vector3(x:h, y:0, z:v);
 rb.AddForce(direcao * velocidade * Time.deltaTime, ForceMode.Impulse);
+if(Input.GetKeyDown(KeyCode.Space))
+{
+    rb.AddForce(Vector3.up * forcaPulo,ForceMode.Impulse);
+}
+
+
 if (transform.position.y < -5)
 {
      SceneManager.LoadScene(SceneManager.GetActiveScene().name);
